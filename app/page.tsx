f847e19b95e9ae606b280ba09771a2a7c2eebe22
tarunreddybabi <tarunreddy.b@enterpi.com>
2025-05-26 12:47:32 +0530

    basic screens are done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import Cta from "@/components/CTA";
+import { recentSessions } from "@/constants";
 
 const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <main>
+      <h1>Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="Science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="Maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="language"
+          subject="English Literature"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+      <section className="home-section">
+        <CompanionsList title="Recently completed sessions" companions={recentSessions} classNames="w-2/3 max-lg:w-full" />
+        <Cta />
+      </section>
+    </main>
+  );
+};
 
-export default Page
+export default Page;
