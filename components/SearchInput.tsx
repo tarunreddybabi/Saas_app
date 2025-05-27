7fd2948428d8b49078e0001b0e2adb7bbbce114d
tarunreddybabi <tarunreddy.b@enterpi.com>
2025-05-27 12:34:42 +0530

    added filters in companions page

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+"use client";
+
+import Image from "next/image";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { formUrlQuery, removeKeysFromUrlQuery } from "@jsmastery/utils";
+import path from "path";
 
 const SearchInput = () => {
-  return (
-    <div>SearchInput</div>
-  )
-}
+  const pathname = usePathname();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const query = searchParams.get("topic") || "";
 
-export default SearchInput
+  const [searchQuery, setSearchQuery] = useState("");
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (searchQuery) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "topic",
+          value: searchQuery,
+        });
+
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (pathname === "/companions") {
+          const newUrl = removeKeysFromUrlQuery({
+            params: searchParams.toString(),
+            keysToRemove: ["topic"],
+          });
+
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    },500);
+  }, [searchQuery, router, searchParams, pathname]);
+
+  return (
+    <div className="relative border border-black rounded-lg items-center flex gap-2 px-2 py-1 h-fit">
+      <Image src="/icons/search.svg" alt="search" width={15} height={15} />
+      <input
+        placeholder="Search companions..."
+        className="outline-none"
+        value={searchQuery}
+        onChange={(e) => {
+          setSearchQuery(e.target.value);
+        }}
+      />
+    </div>
+  );
+};
+
+export default SearchInput;
